ab22ce7cf890e7789f270410d3f8ae6a330d87cf
andrew.hani <andrew.lawendy@outlook.com>
2019-03-03 22:30:14 +0200

    Starting the library

---
+++ js/jReflect.js
@@ -0,0 +1,9 @@
+(function(_this){
+    function initReflect(){
+        let jReflect = {};
+        return jReflect;
+    }
+    if(typeof jReflect == "undefined"){
+        _this.jReflect = initReflect();
+    }
+})(window);
---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function () {
+    let miyazakiCharacters = [{
+        name: "Kiki",
+        film: "Kiki's Delivery Service",
+        year: 1989,
+        synopsis: "In this anime feature, 13-year-old Kiki moves to a seaside town with her talking cat, Jiji, to spend a year alone, in accordance with her village's tradition for witches in training. After learning to control her broomstick, Kiki sets up a flying courier service and soon becomes a fixture in the community. But when the insecure young witch begins questioning herself and loses her magic abilities, she must overcome her self-doubt to get her powers back."
+    }];
+})()
