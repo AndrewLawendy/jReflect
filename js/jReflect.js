74e162092ed388ad199e0e270a4915d5b9a7f2e1
andrew.hani <andrew.lawendy@outlook.com>
2019-04-29 19:21:49 +0200

    Basic Methods

--- js/jReflect.js
+++ js/jReflect.js
@@ -1,9 +1,70 @@
-(function(_this){
-    function initReflect(){
-        let jReflect = {};
-        return jReflect;
+(function (window) {
+    'use strict';
+    var jRef = window.jReflect = (window.jReflect = {});
+
+    function domIsReady(fn) {
+        function trigger() {
+            window.document.removeEventListener('DOMContentLoaded', trigger);
+            window.removeEventListener('load', trigger);
+            fn();
+        }
+
+        // check if document is already loaded
+        if (window.document.readyState === 'complete') {
+            window.setTimeout(fn);
+        } else {
+            // We can not use jqLite since we are not done loading and jQuery could be loaded later.
+
+            // Works for modern browsers and IE9
+            window.document.addEventListener('DOMContentLoaded', trigger);
+
+            // Fallback to window.onload for others
+            window.addEventListener('load', trigger);
+        }
     }
-    if(typeof jReflect == "undefined"){
-        _this.jReflect = initReflect();
+    jRef.init = function () {
+        var scopes = document.querySelectorAll('[jrf-scope]');
+        [].forEach.call(scopes, function (scope) {
+            var scopeArr = scope.getAttribute('jrf-scope');
+            if (window[scopeArr]) jRef.compile(scope, window[scopeArr]);
+        });
     }
+    jRef.compile = function (scope, arr) {
+        var repeatElms = scope.querySelectorAll('[jrf-repeat]');
+        // jRef.assignSpots(repeatElms);
+        [].forEach.call(repeatElms, function (el, index) {
+            // jRef.innerRepeat(el, arr);
+            var parent = el.parentNode,
+                repeatVal = el.getAttribute("jrf-repeat"),
+                repeatKeys = repeatVal.split(' in '),
+                item = repeatKeys[0],
+                repeaterName = repeatKeys[1],
+                repeater = jRef.getRepeater(arr, repeaterName),
+                docFr = document.createDocumentFragment();
+            parent.removeChild(el);
+            repeater.forEach(function (r, index) {
+                var comment = document.createComment(" Repeat: " + repeatVal + " ");
+                docFr.appendChild(comment);
+                docFr.append(el.cloneNode(true));
+            });
+            parent.appendChild(docFr);
+        });
+    }
+    // jRef.innerRepeat = function (el, arr) {
+    //     var innerRepeat = el.querySelectorAll('[jrf-repeat]');
+    //     [].forEach.call(innerRepeat, function (scope) {
+    //         jRef.compile(scope, arr);
+    //     });
+    // }
+    jRef.getRepeater = function (arr, repeaterName) {
+        if (arr.hasOwnProperty(repeaterName))
+            return arr[repeaterName];
+        switch (repeaterName) {
+            case "$root":
+                return arr;
+            default:
+                return arr;
+        }
+    }
+    domIsReady(jRef.init);
 })(window);
